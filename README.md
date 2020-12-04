6686189d89d88f7f0b3d5b8352b9a6457d341884
Matt Gibson <contact.matt.gibson@gmail.com>
2020-12-04 15:21:23 -0800

    update with new theme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a demo website to explore the use of the blogdown package in R.
--- main.R
+++ main.R
@@ -3,3 +3,15 @@ setwd("/Users/matt/projects/blogdown_demo/")
 
 # create the site
 blogdown::new_site()
+
+# add a minimal theme
+blogdown::install_theme("yihui/hugo-xmin")
+
+# now need to modify config.toml with appropriate themes
+# e.g. subl config.toml 
+
+# build the website into public/
+blogdown::hugo_build()
+
+# push the code to git and hopefully you've setup 
+# an appropriate netlify build step.
